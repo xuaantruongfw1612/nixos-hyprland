daa70646b7fe0f563b152d223e0a4f6c9b5bdb6f
Xuân Trường <alonesera1612@gmail.com>
2026-01-09 16:10:55 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,34 +1,28 @@
 {
-  description = "Hyprland on NixOS";
+  description = "NixOS Hyprland + Home Manager (no theme)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-  {
-    nixosConfigurations.hyprland-btw = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
 
       modules = [
         ./configuration.nix
-
         home-manager.nixosModules.home-manager
-
         {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-
-            users.truong = import ./home.nix;
-
-            backupFileExtension = "backup";
-          };
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.truong = import ./home.nix;
         }
       ];
     };
