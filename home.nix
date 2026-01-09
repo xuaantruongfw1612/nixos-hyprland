ad7cd5d78767ee86aaddb25c44274e6dbbc7779f
Xuân Trường <alonesera1612@gmail.com>
2026-01-09 17:02:33 +0700

    Update home.nix

--- home.nix
+++ home.nix
@@ -5,27 +5,209 @@
   home.homeDirectory = "/home/truong";
   home.stateVersion = "25.11";
 
+  # Additional packages
+  home.packages = with pkgs; [
+    brightnessctl
+    pamixer
+    playerctl
+    networkmanagerapplet
+    fcitx5
+    fcitx5-unikey
+  ];
+
   wayland.windowManager.hyprland = {
     enable = true;
 
     settings = {
       "$mod" = "SUPER";
 
+      # Monitor configuration
       monitor = [
         ",preferred,auto,1"
       ];
 
+      # Autostart
       exec-once = [
         "mako"
         "hyprpaper"
         "fcitx5 -d"
+        "nm-applet"
       ];
 
+      # Input configuration
+      input = {
+        kb_layout = "us";
+        follow_mouse = 1;
+
+        touchpad = {
+          natural_scroll = true;
+          disable_while_typing = true;
+        };
+
+        sensitivity = 0;
+      };
+
+      # General appearance
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+        border_size = 2;
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+        layout = "dwindle";
+      };
+
+      # Decorations
+      decoration = {
+        rounding = 10;
+
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+        };
+
+        drop_shadow = true;
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+      };
+
+      # Animations
+      animations = {
+        enabled = true;
+
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+        animation = [
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "borderangle, 1, 8, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
+
+      # Layout configuration
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+      };
+
+      # Keybindings
       bind = [
+        # Applications
         "$mod, RETURN, exec, foot"
-        "$mod, Q, killactive"
         "$mod, D, exec, wofi --show drun"
+
+        # Window management
+        "$mod, Q, killactive"
+        "$mod, F, fullscreen, 0"
+        "$mod, V, togglefloating"
+        "$mod, P, pseudo"
+        "$mod, S, togglesplit"
+
+        # Focus movement (vim keys)
+        "$mod, h, movefocus, l"
+        "$mod, l, movefocus, r"
+        "$mod, k, movefocus, u"
+        "$mod, j, movefocus, d"
+
+        # Focus movement (arrow keys)
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+
+        # Move windows (vim keys)
+        "$mod SHIFT, h, movewindow, l"
+        "$mod SHIFT, l, movewindow, r"
+        "$mod SHIFT, k, movewindow, u"
+        "$mod SHIFT, j, movewindow, d"
+
+        # Move windows (arrow keys)
+        "$mod SHIFT, left, movewindow, l"
+        "$mod SHIFT, right, movewindow, r"
+        "$mod SHIFT, up, movewindow, u"
+        "$mod SHIFT, down, movewindow, d"
+
+        # Workspace switching
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
+
+        # Move to workspace
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 10"
+
+        # Special workspace (scratchpad)
+        "$mod, grave, togglespecialworkspace, magic"
+        "$mod SHIFT, grave, movetoworkspace, special:magic"
+
+        # Scroll through workspaces
+        "$mod, mouse_down, workspace, e+1"
+        "$mod, mouse_up, workspace, e-1"
+
+        # Screenshot
+        ", Print, exec, grim -g \"$(slurp)\" - | wl-copy"
+        "$mod, Print, exec, grim - | wl-copy"
+        "SHIFT, Print, exec, grim -g \"$(slurp)\" ~/Pictures/screenshot-$(date +%Y%m%d-%H%M%S).png"
+
+        # Media controls
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPause, exec, playerctl play-pause"
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPrev, exec, playerctl previous"
+
+        # Volume controls
+        ", XF86AudioRaiseVolume, exec, pamixer -i 5"
+        ", XF86AudioLowerVolume, exec, pamixer -d 5"
+        ", XF86AudioMute, exec, pamixer -t"
+
+        # Brightness controls
+        ", XF86MonBrightnessUp, exec, brightnessctl set 5%+"
+        ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
+
+        # Exit Hyprland
+        "$mod SHIFT, E, exit"
+      ];
+
+      # Resize windows (hold to repeat)
+      binde = [
+        "$mod CTRL, h, resizeactive, -20 0"
+        "$mod CTRL, l, resizeactive, 20 0"
+        "$mod CTRL, k, resizeactive, 0 -20"
+        "$mod CTRL, j, resizeactive, 0 20"
+
+        "$mod CTRL, left, resizeactive, -20 0"
+        "$mod CTRL, right, resizeactive, 20 0"
+        "$mod CTRL, up, resizeactive, 0 -20"
+        "$mod CTRL, down, resizeactive, 0 20"
+      ];
+
+      # Mouse bindings
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
       ];
     };
   };
+
+  programs.home-manager.enable = true;
 }
